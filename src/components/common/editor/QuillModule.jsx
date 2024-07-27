be1af60112510abe29d769ea5770ade7be32e77e
KIMHT98 <kht19684@gmail.com>
2024-07-27 23:16:01 +0900

    FEAT:reace-quill로 editor변경

---
+++ src/components/common/editor/QuillModule.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+export default function QuillModule({ image }) {
+  return (
+    <div id="toolbar">
+      <span className="ql-formats">
+        <select
+          className="ql-size"
+          defaultValue="medium"
+        >
+          <option value="small">Small</option>
+          <option value="medium">Medium</option>
+          <option value="large">Large</option>
+          <option value="huge">Huge</option>
+        </select>
+        <select className="ql-header">
+          <option value="1">Header 1</option>
+          <option value="2">Header 2</option>
+          <option value="3">Header 3</option>
+          <option value="4">Header 4</option>
+          <option value="5">Header 5</option>
+          <option value="6">Header 6</option>
+        </select>
+      </span>
+      <span className="ql-formats">
+        <button className="ql-bold" />
+        <button className="ql-italic" />
+        <button className="ql-underline" />
+        <button className="ql-strike" />
+        <button className="ql-blockquote" />
+      </span>
+      <span className="ql-formats">
+        <select className="ql-color" />
+        <select className="ql-background" />
+      </span>
+      <span className="ql-formats">
+        {image && <button className="ql-image" />}
+        <button className="ql-video" />
+      </span>
+      <span className="ql-formats">
+        <button className="ql-clean" />
+      </span>
+    </div>
+  );
+}
--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme";
-
+import Modal from "react-modal";
 import AppRouter from "./router/AppRouter";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+Modal.setAppElement("#root");
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
